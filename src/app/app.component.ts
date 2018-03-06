c178a9fc16f06ec310c8265a23acfec850ccd30a
Shahar Rosen <ps3tokyros@gmail.com>
2018-03-06 22:38:45 +0200

    Client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthorizationService} from "./shared/authorization.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private auth: AuthorizationService){}
+
+  ngOnInit(){
+    this.auth.login("ShaharR", "123456");
+  }
+
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {SharedModule} from "./shared/shared.module";
+import {Routes, RouterModule} from "@angular/router";
+import {NotFoundComponent} from "./shared/not-found/not-found.component";
+import {LoginComponent} from "./shared/login/login.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {DashboardComponent} from "./dashboard/dashboard/dashboard.component";
+import {DashboardModule} from "./dashboard/dashboard.module";
 
+
+const appRoutes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'main', component: DashboardComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '**', component: NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    BrowserAnimationsModule,
+    DashboardModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
